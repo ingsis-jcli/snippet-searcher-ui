019f7a8e0754578125c75d67c3a960ba58221503
ClaraLopez1 <mclopez@mail.austral.edu.ar>
2024-10-29 09:27:50 -0300

    volviendo atras cambios que no funcionaron

--- src/global.d.ts
+++
@@ -1,6 +0,0 @@
-interface Window {
-    env: {
-        VITE_AUTH0_DOMAIN: string;
-        VITE_AUTH0_CLIENT_ID: string;
-    };
-}
--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,11 @@ import {PaginationProvider} from "./contexts/paginationProvider.tsx";
 import {SnackbarProvider} from "./contexts/snackbarProvider.tsx";
 import {Auth0Provider} from "@auth0/auth0-react";
 
-const auth0Domain = window.env?.VITE_AUTH0_DOMAIN || '';
-const auth0ClientId = window.env?.VITE_AUTH0_CLIENT_ID || '';
-
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Auth0Provider
-            domain={auth0Domain}
-            clientId={auth0ClientId}
+            domain={import.meta.env.VITE_AUTH0_DOMAIN ?? ""}
+            clientId={import.meta.env.VITE_AUTH0_CLIENT_ID ?? ""}
             authorizationParams={{
                 redirect_uri: window.location.origin,
                 audience:"snippet-searcher",
